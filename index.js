b550cf3ad289eb4830f2dfe4f968ba431ce3c004
Andy <andrea_sue2@icloud.com>
2021-12-06 15:56:01 -0600

    cleaned up valid choice logic a bit; should this be an imported helper function?

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const input = require('readline-sync');
 const tweetLink = require('./src/tweet-link');
 
-function runProgram() {
+validChoices = ['a', 'b'];
+
+askForTypeOfTask = () => {
   userChoice = input.question(
     `What would you like to do? \n Type A for tweet a link \n Type B for something else \n`
   );
-  if (userChoice.toLowerCase() === 'a') {
+  return userChoice;
+};
+
+isValidChoice = (userInput) => {
+  if (
+    userInput.length > 1 ||
+    typeof userInput !== 'string' ||
+    !validChoices.includes(userInput)
+  ) {
+    console.log(`invalid choice`);
+    return false;
+  } else {
+    console.log(`valid choice`);
+    return true;
+  }
+};
+
+runProgram = () => {
+  userChoice = askForTypeOfTask().toLowerCase();
+
+  while (!isValidChoice(userChoice)) {
+    userChoice = askForTypeOfTask().toLowerCase();
+  }
+
+  if (userChoice === 'a') {
     tweetLink();
   } else {
     console.log(`picked something else`);
   }
-}
+};
 
 runProgram();
