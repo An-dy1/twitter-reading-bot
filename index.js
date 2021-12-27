3217b3bdcbffca30a599a88dcdaeabdfc7703d89
Andy <andrea_sue2@icloud.com>
2021-12-27 16:14:28 -0600

    update back to running the twitter bot

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send(`Hello, World`))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.send(`Hello, World`))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 validChoices = ['a', 'b'];
 
@@ -48,7 +48,7 @@ runProgram = () => {
   }
 };
 
-// runProgram();
+runProgram();
 
 // note: this is not being called anywhere in this program at the moment
 // see readme for more
